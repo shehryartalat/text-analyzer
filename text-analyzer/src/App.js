f301dd2132356ab6d4f06a4b02e11452722356c4
shehryartalat <shehryartalal41@gmail.com>
2022-09-19 16:25:14 +0500

    Dark or light mode

--- text-analyzer/src/App.js
+++ text-analyzer/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function App() {
   const [value, setValue] = useState('')
@@ -9,10 +9,12 @@ function App() {
   const [sentence, setsentence] = useState(0)
   const [copy, setcopy] = useState(0)
   const [clear, setclear] = useState(null)
-  const [lenghtShow,setLengthShow] = useState(false)
-  const [countshow,setcountshow] = useState(false)
-  const [sentenceshow,setsentenceshow] = useState(false)
-  const [clears] = useState(false)
+  const [lenghtShow, setLengthShow] = useState(false)
+  const [countshow, setcountshow] = useState(false)
+  const [sentenceshow, setsentenceshow] = useState(false)
+  const [clears, setclears] = useState(false)
+  const [mode, setMode] = useState(false)
+  const [shift, setshift] = useState(false)
 
 
   const changeValue = (e) => {
@@ -56,28 +58,74 @@ function App() {
   };
 
 
+  // useEffect(()=>{},[])
+
+
+
+
+  const changeMode = () => {
+
+
+    if (mode === false)
+      setMode(true);
+    else
+      setMode(false)
+  }
+
+  const changeshift = () => {
+
+    if (shift === false)
+      setshift(true);
+    else
+      setshift(false)
+  }
+
 
 
 
   return (
-    <div className="App container">
-      <h1 className='display-1'>Text-Analyzer</h1>
+    <div className={`App  ${mode !== true ? 'dark' : 'light'}`}>
+      <nav className={`navbar navbar-expand-lg navbar-light bg-${mode === true ? 'light' : 'dark'} text-${mode !== true ? 'light' : 'dark'}`}>
+        <div className="container-fluid">
+          <a className="navbar-brand">Navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
-      {/* <input className='form-control my-4' onChange={changeValue} value={value}></input> */}
+            </ul>
+            <form className="d-flex">
+              <div className="form-check form-switch">
+                <input className="form-check-input" type="checkbox"
+                  onChange={changeMode}
+                  role="switch" id="flexSwitchCheckDefault"></input>
+                <label className="form-check-label">{mode === true ? 'Dark mode' : 'light mode'}</label>
+              </div>
 
-      <textarea className='form-control my-4' onChange={changeValue} value={value} id="exampleFormControlTextarea1" rows="3"></textarea>
-      {/* <button type='button' className='btn btn-primary' onClick={findLength}>Calculate length</button>
-      <button type="button" className='btn btn-secondary'>Calculate words</button> */}
+            </form>
+          </div>
+        </div>
+      </nav>
 
-      <button type="button" className="btn btn-primary me-md-2" onClick={findLength}>Calculate length</button>
-      <button type="button" className="btn btn-secondary me-md-2" onClick={findcount}>Calculate words</button>
-      <button type='button' className='btn btn-success me-md-2' onClick={findsentence}>Calculate sentences</button>
-      <button type='button' className='btn btn-warning me-md-2' onClick={copytoboard}>Copy to clipboard</button>
-      <button type='button' className='btn btn-danger me-md-2' onClick={clearfield}>Clear</button>
 
-      {lenghtShow == true? <p>The length is {length}</p> : ''}
-      {countshow == true? <p>The words are {count}</p> : ''}
-      {sentenceshow == true? <p>The sentences are {sentence}</p> : ''}
+      {/* <h1 className='display-1'>Text-Analyzer</h1> */}
+
+      <div className='container'>
+        <textarea className={`form-control my-4 bg-${mode === true ? 'light' : 'dark'} text-${mode !== true ? 'light' : 'dark'}`} onChange={changeValue} value={value} id="exampleFormControlTextarea1" rows="3"></textarea>
+
+        <button type="button" className="btn btn-primary me-md-2" onClick={findLength}>Calculate length</button>
+        <button type="button" className="btn btn-secondary me-md-2" onClick={findcount}>Calculate words</button>
+        <button type='button' className='btn btn-success me-md-2' onClick={findsentence}>Calculate sentences</button>
+        <button type='button' className='btn btn-warning me-md-2' onClick={copytoboard}>Copy to clipboard</button>
+        <button type='button' className='btn btn-danger me-md-2' onClick={clearfield}>Clear</button>
+
+        {lenghtShow == true ? <p>The length is {length}</p> : ''}
+        {countshow == true ? <p>The words are {count}</p> : ''}
+        {sentenceshow == true ? <p>The sentences are {sentence}</p> : ''}
+
+
+      </div>
 
 
     </div>
